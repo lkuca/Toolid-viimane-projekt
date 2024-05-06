7d13796b0096c085ce76fb34cb1f0dc821fedd83
Anton Buivol <anton.buivol@gmail.com>
2024-05-06 10:01:24 +0300

    Muudalisa uus background Image

--- conf.php
+++ conf.php
@@ -1,8 +1,8 @@
 <?php
-$kasutaja='d123175_luca';
-$sererinimi='d123175.mysql.zonevs.eu';
-$parool='pentagonanetu27';
-$andmebaas='d123175_andmebaas';
+$kasutaja='antonbuivol';
+$sererinimi='localhost';
+$parool='123456';
+$andmebaas='antonbuivol';
 $yhendus=new mysqli($sererinimi,$kasutaja,$parool,$andmebaas);
 $yhendus->set_charset('UTF8');
 ?>
